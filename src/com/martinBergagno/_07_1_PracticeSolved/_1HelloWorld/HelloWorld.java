f25a32598a8f455f276c4a1397ad2a7258eec3e9
martin-b-07e5 <94968383+martin-b-07e5@users.noreply.github.com>
2022-06-12 00:29:48 -0300

    Delete src/com/martinBergagno/_07_1_PracticeSolved directory

--- src/com/martinBergagno/_07_1_PracticeSolved/_1HelloWorld/HelloWorld.java
+++
@@ -1,7 +0,0 @@
-package com.martinBergagno._07_1_PracticeSolved._1HelloWorld;
-
-public class HelloWorld {
-  public static void main(String[] args) {
-    System.out.println("Hello World");
-  }
-}
