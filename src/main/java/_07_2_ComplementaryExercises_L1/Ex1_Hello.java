a19e851a02388eccae50b0b29692d56e787464dd
martin-b-07e5 <martin.bergagno@gmail.com>
2022-06-29 21:58:41 -0300

    method toUpperCase() removed

--- src/main/java/_07_2_ComplementaryExercises_L1/Ex1_Hello.java
+++ src/main/java/_07_2_ComplementaryExercises_L1/Ex1_Hello.java
@@ -14,9 +14,9 @@ public class Ex1_Hello {
     System.out.println("Input name");
     Scanner scanner = new Scanner(System.in);
     String name = scanner.nextLine();
-   scanner.close();
+    scanner.close();
 
-    System.out.println("HELLO " + name.toUpperCase() + "!!!");
+    System.out.println("HELLO " + name + "!!!");
 
   }
 
